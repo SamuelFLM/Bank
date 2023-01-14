36def8153ef7db3786c8291805295c17d4d0bca3
SamuelFLM <felipelimaaa23@gmail.com>
2023-01-14 16:28:08 -0300

    Banco de dados - Registro

--- src/banco de dados/conexao.py
+++ src/banco de dados/conexao.py
@@ -5,9 +5,9 @@ class Connection:
     def __init__(self):
         self.server = "localhost\SQLEXPRESS"
         self.database = "LOGIN"
-
-        cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+self.server+';DATABASE='+self.database+';Trusted_Connection=yes;')
-        cursor = cnxn.cursor()
+        self.cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+self.server+';DATABASE='+self.database+';Trusted_Connection=yes;') 
+        self.cursor = self.cnxn.cursor()
 
 if __name__ == "__main__":
+    # OK 
     conexao = Connection()
---
+++ src/banco de dados/register.py
@@ -0,0 +1,20 @@
+from conexao import Connection
+
+class Register:
+    
+    def __init__(self, email, senha):
+        self.conexao = Connection()
+        self.email = email
+        self.senha = senha
+        
+    def registro(self):
+        _sql_inserir_dados = f"""
+        INSERT INTO REGISTRO VALUES('{self.email}', '{self.senha}')
+        """
+        self.conexao.cursor.execute(_sql_inserir_dados)
+        self.conexao.cursor.commit()
+        
+if __name__ == "__main__":
+    # OK
+    reg = Register("email@gmail.com", "1234567")
+    reg.registro()
