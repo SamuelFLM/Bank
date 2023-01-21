813cba05770284cf5845c6831f77f6c0cc3a6d61
SamuelFLM <felipelimaaa23@gmail.com>
2023-01-20 22:05:38 -0300

    Att estrutura page Ajuda

--- src/Conf/Confg_ajuda.py
+++ src/Conf/Confg_ajuda.py
@@ -1,8 +1,8 @@
 import PySimpleGUI as sg
 import re
-from Page.Interface_ajuda import *
+from Page.Interface_ajuda import Page_ajudar
 ## Arruma bug
-window = 1
+window = Page_ajudar.ajudar()
 
 while True:    
     event, values =window.read()
@@ -13,12 +13,14 @@ while True:
         email = values["email"]
             
         validacao = re.search(r'^[a-zA-Z0-9._-]+@[a-zA-Z0-9]+\.[a-zA-Z\.a-zA-Z]{1,3}$', email)
-        if validacao:
+        if validacao and bool(values["comentario"]):
             window["email_titulo"].update(filename="img\img ajuda\Icon\E-mail.png")
             window["line_rosa"].update(filename="img\img ajuda\Icon\Line.png")
+            window["rodape"].update(filename="img\img ajuda\Icon\Rodapeon.png")
         else:
             window["email_titulo"].update(filename="img\img ajuda\Icon\Verifique o e-mail digitado.png")
             window["line_rosa"].update(filename="img\img ajuda\Icon\Line Rosa.png")
+            window["rodape"].update(filename="img\img ajuda\Icon\Rodapeoff.png")
         # contador = len(values["comentario"])
         # window["caracteres"].update(f"{contador}/ 50 caracteres")
     if event == "rodape":
