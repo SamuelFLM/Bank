6ed602ad3a31fa0053e51c0d160c30094715a9f6
SamuelFLM <felipelimaaa23@gmail.com>
2023-02-07 22:00:33 -0300

    Att project

---
+++ src/Conf/Page/Interface_cadastro.py
@@ -0,0 +1,14 @@
+import PySimpleGUI as sg
+
+
+def cadastro():
+    
+    cabecalho = []
+    logo = []
+    dados_nome_sobrenome = []
+    dados_email = []
+    dados_cpf = []
+    dados_data_nascimento = []
+    rodape = []
+    
+    
--- src/Conf/Page/Interface_home.py
+++ src/Conf/Page/Interface_home.py
@@ -1,20 +1,27 @@
 import PySimpleGUI as sg
+from Interface_login import *
+from Interface_cadastro import *
+from Interface_ajuda import *
 
-
-sg.theme_background_color("#0A1B1E")
 def home():
-    cabecalho = [sg.Image(filename="img\img home\Logo\logo2.png", pad=(0,(0,0)), background_color=sg.theme_background_color())]
+    cabecalho = [sg.Image(filename="img\img home\Logo\logo2.png", pad=(0,(0,0)), background_color=sg.theme_background_color("#0A1B1E") ,enable_events=True, key="ajuda")]
     opcao = [
-        [sg.Image(filename="img\img home\Icon\Invista com Taxa Zero.png", pad=(0,(60,0)), background_color=sg.theme_background_color())],
-        [sg.Image(filename="img\img home\Icon\soucliente.png", pad=(0,(30,0)), background_color=sg.theme_background_color())],
-        [sg.Image(filename="img\img home\Icon\criarconta.png", pad=(0,(0,0)), background_color=sg.theme_background_color())],
+        [sg.Image(filename="img\img home\Icon\Invista com Taxa Zero.png", pad=(0,(60,0)), background_color=sg.theme_background_color("#0A1B1E"))],
+        [sg.Image(filename="img\img home\Icon\soucliente.png", pad=(0,(30,0)), background_color=sg.theme_background_color("#0A1B1E"), enable_events=True, key="login")],
+        [sg.Image(filename="img\img home\Icon\criarconta.png", pad=(0,(0,0)), background_color=sg.theme_background_color("#0A1B1E"),enable_events=True, key="cadastro")],
     ]
     
     layout = [cabecalho, opcao]
-    window = sg.Window("Home", layout, size=(410, 750), margins=(0,0), element_justification="c")
+    window = sg.Window("Home", layout, size=(410, 750), margins=(0,0), element_justification="c", icon="img//logo.ico")
     
     while True:
         event, values = window.read()
         if event == sg.WIN_CLOSED:
+            break
+        if event == "login":
+            a()
+        if event == "ajuda":
+            window.close()
+            ajudar(home=home)
             break
 home()
