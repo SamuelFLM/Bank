d0656e3016cfa1fb72b113e07d6f98f742b9a405
SamuelFLM <felipelimaaa23@gmail.com>
2023-02-09 13:57:15 -0300

    Att tela aprovado

--- src/Conf/Page/Interface_aprovado.py
+++ src/Conf/Page/Interface_aprovado.py
@@ -2,23 +2,23 @@ import PySimpleGUI as sg
 
 def aprovado():
     sg.theme_background_color("white")
-    cabecalho = [sg.Image(filename="img//img aprovado//icon//Group 20.png", background_color="white")]
-    titulo  = [sg.Image(filename="img//img aprovado//icon//Olá, sua conta foi aprovada com sucesso!!.png", background_color="white")]
-    mensagem = [sg.Image(filename="img//img aprovado//icon//Use os dados abaixo para efetuar o login no app.png", background_color="white")]
-    logo = [sg.Image(filename="img//img aprovado//icon//logo-itau-256 3.png", background_color="white")]
+    cabecalho = [sg.Image(filename="img//img aprovado//icon//Group 20.png", background_color="white", pad=(20,(30,0)))]
+    titulo  = [sg.Image(filename="img//img aprovado//icon//Olá, sua conta foi aprovada com sucesso!!.png", background_color="white", pad=(25,(60,50)))]
+    mensagem = [sg.Image(filename="img//img aprovado//icon//Use os dados abaixo para efetuar o login no app.png", background_color="white", pad=(25,(0,50)))]
+    logo = [sg.Image(filename="img//img aprovado//icon//logo-itau-256 3.png", background_color="white", pad=(15,(0,50)))]
     
     dados_agencia_conta_corrente = [
-        [sg.Image(filename="img//img aprovado//icon//Agência.png", background_color="white"), sg.Image(filename="img//img aprovado//icon//Conta corrente.png", background_color="white")],
-        [sg.Input("",background_color="white", border_width=0,k="agencia", s=(10,0), pad=(0,(20,0)),font="Inter 13"), sg.Input("",background_color="white", border_width=0,k="contacorrente", s=(10,0), pad=(30,(20,0)),font="Inter 13")],
-        [sg.Image(filename="img//img aprovado//icon//Line 11.png", background_color="white"),sg.Image(filename="img//img aprovado//icon//Line 11.png", background_color="white")]
+        [sg.Image(filename="img//img aprovado//icon//Agência.png", background_color="white",pad=(20,(0,20))), sg.Image(filename="img//img aprovado//icon//Conta corrente.png", background_color="white", pad=(100,(0,20)))],
+        [sg.Input("",background_color="white", border_width=0,k="agencia",disabled=True, s=(10,0), pad=(20,(0,0)),font="Inter 13"), sg.Input("",background_color="white",disabled=True ,border_width=0,k="contacorrente", s=(10,0), pad=(65,(0,0)),font="Inter 13")],
+        [sg.Image(filename="img//img aprovado//icon//Line 12 (1).png", background_color="white",pad=(20,(0,0))),sg.Image(filename="img//img aprovado//icon//Line 12 (1).png", background_color="white", pad=(10,(0,0)))]
     ]
     senha_eletronica = [
-        [sg.Image(filename="img//img aprovado//icon//Senha eletrônica.png", background_color="white")],
-        [sg.Input("",background_color="white", border_width=0,k="agencia", s=(10,0), pad=(0,(20,0)),font="Inter 13")],
-        [sg.Image(filename="img//img aprovado//icon//Line 13.png", background_color="white")]
+        [sg.Image(filename="img//img aprovado//icon//Senha eletrônica.png", background_color="white", pad=(20,(50,0)))],
+        [sg.Input("",background_color="white", border_width=0,k="senha_eletronica",disabled=True, s=(10,0), pad=(20,(20,0)),font="Inter 13")],
+        [sg.Image(filename="img//img aprovado//icon//Line 13.png", background_color="white", pad=(20,(0,100)))]
         
     ]
-    rodape = [[sg.Image(filename="img//img aprovado//icon//rodape.png", background_color="white" , pad=(0,(0,0)))]]
+    rodape = [[sg.Image(filename="img//img aprovado//icon//rodape.png", background_color="white" , pad=(0,(30,0)))]]
     
     layout = [cabecalho,titulo,mensagem,logo,dados_agencia_conta_corrente,senha_eletronica,rodape]
     
